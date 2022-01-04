f5f6843de9692cbd47d50896d3db4d1d48bfe5e0
Remy Wang <remywang@cs.washington.edu>
2022-01-04 12:01:07 -0800

    SPNF.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,32 @@ use egg::*;
 use usr::rewrites::*;
 
 fn main() {
-    let start = "(not x)".parse().unwrap();
+    let start = "(sig t1 (sig t2 (sig t3
+        (* ([ (= t2 t)) 
+            (* ([ (= (. t1 k) (. t2 k)))
+                (* ([ (>= (. t1 a) 12)) 
+                    (* ([ (= (. t3 k) (. t1 k))) 
+                        (* ([ (= (. t3 a) (. t1 a))) 
+                            (* (R t3) 
+                                (R t2))))))))))".parse().unwrap();
+    let mid = "(sig t1 (sig t2 
+        (* ([ (= t2 t)) 
+            (* (* ([ (= (. t1 k) (. t2 k)))
+                        ([ (>= (. t1 a) 12))) 
+                        (sig t3 (* (* ([ (= (. t3 k) (. t1 k))) 
+                        (* ([ (= (. t3 a) (. t1 a))) (R t3)))
+                (R t2)))))))".parse().unwrap();
+    let end = "(sig t1 (sig t2 
+        (* ([ (= t2 t)) 
+            (* (sig t3 (* ([ (= (. t3 k) (. t1 k))) 
+                        (* ([ (= (. t3 a) (. t1 a))) (R t3))))
+                (* (R t2) 
+                    (* ([ (= (. t1 k) (. t2 k)))
+                        ([ (>= (. t1 a) 12))))))))".parse().unwrap();
     let runner = Runner::default()
         .with_expr(&start)
+        .with_expr(&mid)
+        .with_expr(&end)
         .run(&rules());
-    let extractor = Extractor::new(&runner.egraph, AstSize);
-    let (_best_cost, best_expr) = extractor.find_best(runner.roots[0]);
-
-    println!("{}", best_expr);
-    println!("{}", runner.egraph.total_size());
-    println!("{:?}", runner.stop_reason.unwrap());
+    assert_eq!(runner.egraph.find(runner.roots[0]), runner.egraph.find(runner.roots[2]));
 }
